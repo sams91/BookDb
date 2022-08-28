be4a6714a3d7c5c7fbfd8ae4a347cb9bcc48f176
sams91 <103255663+sams91@users.noreply.github.com>
2022-08-28 15:18:05 -0500

    Add project files.

---
+++ dbo/Tables/Books.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Books] (
+    [Id]      INT  NOT NULL,
+    [SciFi]   TEXT NULL,
+    [Comedy]  TEXT NULL,
+    [History] TEXT NULL,
+    [Drama]   TEXT NULL,
+    [Science] TEXT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
